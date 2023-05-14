bbe6e24e6c4dbe37b56937e9bc7ebb21bff0e6c6
erwil <rwan.legoff@gmail.com>
2023-05-14 16:43:16 +0200

    just a detail

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import app from './config/app'
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+  console.log(`GPTone is running on port ${port}`)
 })
