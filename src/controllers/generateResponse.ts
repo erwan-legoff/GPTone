df019dd51c73fe921490447f09ceca64062b2551
erwil <rwan.legoff@gmail.com>
2023-05-14 19:59:35 +0200

    adding system capabilities

--- src/controllers/generateResponse.ts
+++ src/controllers/generateResponse.ts
@@ -8,14 +8,14 @@ import {
   ConfigurationParameters,
 } from 'openai'
 import dotenv from 'dotenv'
+import systemEnum from '../enums/SystemEnum'
 
 dotenv.config()
 
 /*Nevermind, I want it in Interface */
-interface Message extends OpenAiRequestMessage {
-  role: OpenAiRoleEnum
-  content: string
-}
+interface Message extends OpenAiRequestMessage {}
+
+const { User: USER_ROLE, Assistant: ASSISTANT_ROLE, System: SYSTEM_ROLE } = OpenAiRoleEnum
 class ValidationError extends Error {
   constructor(message: string) {
     super(message)
@@ -31,10 +31,12 @@ interface ConversationContext {
 class Conversation {
   conversationContext: ConversationContext[]
   currentMessages: Message[]
+  currentSystem: string
 
-  constructor(conversationContext: ConversationContext[], currentMessages: Message[]) {
+  constructor(conversationContext: ConversationContext[], currentMessages: Message[], currentSystem?: string) {
     this.conversationContext = conversationContext
     this.currentMessages = currentMessages
+    this.currentSystem = currentSystem || defaultSystem
   }
 }
 
@@ -50,39 +52,39 @@ const configuration = new Configuration(configurationParams)
 // OpenAIApi initialization
 const openai = new OpenAIApi(configuration)
 
+const modelId = 'gpt-3.5-turbo'
+
+const defaultSystem = systemEnum.COMPOSITOR
+
+// const defaultSystem = `Pre erwan, tu ne réponds qu'en français. Tu es un mec gentil qui essaie de faire de son mieux pour être honnête et dire la vérité. Tu es le genre de personne qui ne supporte pas de dire des choses fausses avec une grande confiance, tu essaies toujours de révoquer tes premières hypothèses avant d'essayer de répondre à quoi que ce soit avec confiance, quand tu ne sais pas tu essaies toujours de répondre tout en disant ton niveau de certitude.`
+
 // Controller function to handle chat conversation
 export const generateResponse = async (req: Request, res: Response) => {
   try {
-    const { prompt, isNewConversation, pseudo } = req.body
-    const modelId = 'gpt-3.5-turbo'
+    const { prompt, isNewConversation, pseudo, randomness, richness, aiPersonality } = req.body
+
     const promptText = `${prompt}\n\nResponse:`
 
-    const conversationId = handleConversationCreation(isNewConversation, req, pseudo)
+    const conversationId = handleConversation(isNewConversation, req, pseudo, aiPersonality)
 
     const currentConversation = allConversations.get(conversationId) // On sait que la conversation existe
     //Mais on sait jamais
     if (!currentConversation) {
       throw new ValidationError('Conversation could not be created')
     }
-    const { conversationContext, currentMessages } = currentConversation
+
+    if (randomness > 1 || randomness < 0) throw new ValidationError('Randomness must be between 0 and 1')
+    if (richness > 2 || richness < -2) throw new ValidationError('Richness must be between -2 and 2')
+
+    const topProbability = randomness || 0.6
+    const frequencyPenalty = richness || 0.7
 
     // Restore the previous context
-    for (const { prompt, response } of conversationContext) {
-      currentMessages.push({ role: 'user', content: prompt })
-      currentMessages.push({ role: 'assistant', content: response })
-    }
+    generateContext(currentConversation, promptText, pseudo)
 
-    // Stores the new message
-    currentMessages.push({ role: 'user', content: promptText })
+    const { conversationContext, currentMessages } = currentConversation
 
-    const chatCompletionRequest: CreateChatCompletionRequest = {
-      model: modelId,
-      messages: currentMessages,
-    }
-
-    const aiResponse = await openai.createChatCompletion(chatCompletionRequest)
-
-    const aiResponseText = aiResponse.data.choices.shift()?.message?.content || 'No response found'
+    const aiResponseText = await getOpenAiPrediction(currentMessages, topProbability, frequencyPenalty)
     conversationContext.push({ prompt: promptText, response: aiResponseText })
     res.send({ response: aiResponseText, conversationId })
   } catch (err) {
@@ -95,22 +97,66 @@ export const generateResponse = async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Internal server error' })
   }
 }
-function handleConversationCreation(isNewConversation: any, req: Request, pseudo: any) {
+function generateContext(conversation: Conversation, promptText: string, pseudo: string) {
+  const { conversationContext, currentMessages } = conversation
+  for (const { prompt, response } of conversationContext) {
+    currentMessages.push({ role: USER_ROLE, content: prompt, name: pseudo })
+
+    currentMessages.push({ role: ASSISTANT_ROLE, content: response })
+  }
+  const appliedSystem = conversation.currentSystem || defaultSystem
+  if (currentMessages.length > 1) currentMessages.push({ role: SYSTEM_ROLE, content: appliedSystem })
+  // Stores the new message
+  currentMessages.push({ role: 'user', content: promptText })
+}
+
+async function getOpenAiPrediction(currentMessages: Message[], topProbability: number, frequencyPenalty: number) {
+  const chatCompletionRequest: CreateChatCompletionRequest = {
+    model: modelId,
+    messages: currentMessages,
+    top_p: topProbability,
+    frequency_penalty: frequencyPenalty,
+    max_tokens: 20,
+    stop: ['\n'],
+  }
+
+  const aiResponse = await openai.createChatCompletion(chatCompletionRequest)
+
+  const aiResponseText = aiResponse.data.choices.shift()?.message?.content || 'No response found'
+  return aiResponseText
+}
+
+function handleConversation(isNewConversation: any, req: Request, pseudo: any, aiPersonality: string | undefined) {
   const isNewConversationParsed = isNewConversation === 'true' || isNewConversation === true
+  if (!pseudo) throw new ValidationError('Pseudo is required')
+  if (typeof pseudo !== 'string') throw new ValidationError('Pseudo must be a string')
 
   let conversationId = req.body.conversationId
 
-  if (needNewConversation(isNewConversationParsed, conversationId)) {
-    if (!pseudo) throw new ValidationError('Pseudo is required for new conversations')
+  if (conversationId && typeof conversationId !== 'string') throw new ValidationError('ConversationId must be a string')
 
+  // The conversationId should contain the pseudo
+  if (conversationId && !conversationId.includes(pseudo)) throw new ValidationError('Invalid conversationId')
+
+  if (aiPersonality && typeof aiPersonality !== 'string') throw new ValidationError('Personality must be a string')
+
+  if (needNewConversation(isNewConversationParsed, conversationId)) {
+    const appliedSystem = aiPersonality || defaultSystem
     conversationId = generateUniqueId(pseudo)
 
     while (allConversations.has(conversationId)) {
       conversationId = generateUniqueId(pseudo)
     }
-
-    allConversations.set(conversationId, new Conversation([], []))
+    const firstMessage = { role: SYSTEM_ROLE, content: appliedSystem }
+    allConversations.set(conversationId, new Conversation([], [firstMessage], appliedSystem))
   }
+
+  const currentConversation = allConversations.get(conversationId)
+  if (!currentConversation) throw new ValidationError('Conversation could not be created')
+
+  // Modify the current system if needed
+  if (aiPersonality) currentConversation.currentSystem = aiPersonality
+
   return conversationId
 }
 
