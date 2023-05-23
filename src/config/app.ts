3b100175cc1b4c252f963ab29fb38915c2561abf
erwan-legoff <erwan.le-goff@etu.umontpellier.fr>
2023-05-23 20:24:42 +0200

    adding morgan logs

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import { generateResponse } from '../controllers/generateResponse'
-
+import morgan from 'morgan'
 dotenv.config()
 
 const app = express()
 app.use(bodyParser.json())
+app.use(morgan('dev'))
+
 
 app.post('/generate', generateResponse)
 
